3a7ea60218e0aec4b2db3bdad22591e9cf538e78
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-02-05 22:34:36 +0100

    Footer alapváz elkészítése

---
+++ application/views/footer.php
@@ -0,0 +1,11 @@
+    <footer>
+        <div style="text-align: center;">
+            <h5>Önkéntes Portál Nonprofit Kft.</h5>  
+            <p><i class="fas fa-phone"></i> +36 1 200 2020</p>
+            <p><i class="fas fa-envelope"></i> onkentesportal@portal.hu</p>
+            <p><i class="fas fa-map-marked"></i> 1111 Budapest, Budafoki út 59.</p>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
