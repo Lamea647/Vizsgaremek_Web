99a77b4a2b2753ebbaaa5ed60d2394e3f9d53a95
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-02-06 00:24:45 +0100

    Kezdőoldalhoz tartozó controller létrehozása, footer megjelenítésének tesztelése

---
+++ application/controllers/Kezdolap.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kezdolap extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+
+    public function index()
+    {
+        $this->load->view('footer');
+    }
+
+}
+
+
+/* End of file Kezdolap.php */
