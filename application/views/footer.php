a475dc497f8ca0c6508087b17d0de3d92088ea95
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-02-07 19:16:12 +0100

    <br> tag beillesztése

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,6 @@
     <footer>
         <div style="text-align: center;">
-            <h5>Önkéntes Portál Nonprofit Kft.</h5>  
+            <h5>Önkéntes Portál Nonprofit Kft.</h5><br>  
             <p><i class="fas fa-phone"></i> +36 1 200 2020</p>
             <p><i class="fas fa-envelope"></i> onkentesportal@portal.hu</p>
             <p><i class="fas fa-map-marked"></i> 1111 Budapest, Budafoki út 59.</p>
