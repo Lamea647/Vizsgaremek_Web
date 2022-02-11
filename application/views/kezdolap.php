3cece398d5022857428ad32801b6cd7a7c931a3a
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-02-11 22:18:43 +0100

    fooldal.php fájl átnevezése és elmentése kezdolap.php névvel

---
+++ application/views/kezdolap.php
@@ -0,0 +1,29 @@
+<h4 style="text-align:center; font-weight: bold;">Bemutatkozás</h4><br>
+    <div class="row">
+        <div class="col-md-6">
+            <p style="text-align: justify; text-indent: 20px;">
+            Curabitur a orci sed nisi rutrum laoreet. Vestibulum et mattis nisl. Ut ac eros maximus quam interdum lobortis eu at augue. 
+            Aliquam erat volutpat. Suspendisse malesuada molestie tellus, at porta ex imperdiet ut. Fusce pellentesque facilisis varius. Sed viverra facilisis dolor, at mattis purus varius et. Nam dapibus congue pretium. Suspendisse potenti.
+            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse tempus tortor orci, nec elementum neque porta semper. 
+            Donec fringilla, quam a finibus ullamcorper, ex enim tincidunt ex, vestibulum pharetra libero justo vitae mi. Mauris rhoncus sem non orci ultrices sodales. 
+            Integer augue dolor, congue quis ex vel, pharetra semper justo. Quisque nulla lorem, condimentum a ultrices sit amet, tempor quis nibh. 
+            Morbi sagittis feugiat metus sit amet accumsan. Mauris feugiat, velit venenatis rutrum suscipit, sem ligula faucibus dolor, vel semper ipsum risus sit amet quam. 
+            Maecenas at hendrerit turpis, quis vehicula leo. Nunc pellentesque nunc quis consequat sollicitudin. 
+            </p></div>
+        <div class="col-md-6"><img src="<?php echo base_url(); ?>images/bem1.jpg" alt="Bemutatkozó szöveghez tartozó kép." style="width: 400px; height: 300px;" class="img-fluid mx-auto d-block" title="Bemutatkozás"></div>
+    </div><br>
+<h4 style="text-align:center; font-weight: bold;">Szabályzat</h4><br>
+    <div class="row">
+        <div class="col-md-6">
+            <p style="text-align: justify; text-indent: 20px;">
+            Curabitur a orci sed nisi rutrum laoreet. Vestibulum et mattis nisl. Ut ac eros maximus quam interdum lobortis eu at augue. 
+            Aliquam erat volutpat. Suspendisse malesuada molestie tellus, at porta ex imperdiet ut. Fusce pellentesque facilisis varius. Sed viverra facilisis dolor, at mattis purus varius et. Nam dapibus congue pretium. Suspendisse potenti.
+            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse tempus tortor orci, nec elementum neque porta semper. 
+            Donec fringilla, quam a finibus ullamcorper, ex enim tincidunt ex, vestibulum pharetra libero justo vitae mi. Mauris rhoncus sem non orci ultrices sodales. 
+            Integer augue dolor, congue quis ex vel, pharetra semper justo. Quisque nulla lorem, condimentum a ultrices sit amet, tempor quis nibh. 
+            Morbi sagittis feugiat metus sit amet accumsan. Mauris feugiat, velit venenatis rutrum suscipit, sem ligula faucibus dolor, vel semper ipsum risus sit amet quam. 
+            Maecenas at hendrerit turpis, quis vehicula leo. Nunc pellentesque nunc quis consequat sollicitudin. 
+            </p>
+        </div>
+        <div class="col-md-6"><img src="<?php echo base_url(); ?>images/bem2.jpg" alt="Szabályzat szöveghez tartozó kép." style="width: 400px; height: 300px;" class="img-fluid mx-auto d-block" title="Szabályzat"></div>
+    </div><br>
