b8a677687260f5be717775671c966322cc2178bc
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-02-12 15:04:44 +0100

    Hirdetés feladásához model létrehozása, hirdetés kategória lekérdezés elkészítése

---
+++ application/models/Hirdetes_feladas_model.php
@@ -0,0 +1,25 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Hirdetes_feladas_model extends CI_Model {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function kategoria_lista()
+{
+    $this->db->select("kategoria_nev");
+    $query = $this->db->get("kategoria");
+    return $result = $query->result_array();
+}
+
+
+    
+
+}
+
+/* End of file Hirdetes_feladas_model.php */
