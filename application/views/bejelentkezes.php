9dcb3b6f715bfe4a74c84370e2f0f89dea107a60
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-02-12 15:05:49 +0100

    Bejelentkezési felület létrehozása

---
+++ application/views/bejelentkezes.php
@@ -0,0 +1,11 @@
+<form method="post">
+    <div class="form-group">
+        <label for="felhnev">Felhasználónév</label>
+        <input type="text" class="form-control" id="felhnev" name="felhnev" required>
+    </div>
+    <div class="form-group">
+        <label for="jelszo">Jelszó</label>
+        <input type="password" class="form-control" id="jelszo" name="jelszo" required>
+    </div>
+<button type="submit" class="btn btn-warning" name="belepes" value="true">Bejelentkezés</button>
+</form><br>
