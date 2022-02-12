16a7b1ce9c2be1753f1d96b2ff3ee7f2acd7fb1a
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-02-12 15:04:06 +0100

    Hirdetés feladásához controller létrehozása

---
+++ application/controllers/Hirdetes_feladas.php
@@ -0,0 +1,29 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Hirdetes_feladas extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->model('hirdetes_feladas_model');
+    }
+
+    public function hirdetes_feladas(){
+        $kategoria_nev = $this->hirdetes_feladas_model->kategoria_lista();
+        //print_r($kategoria_nev);
+        $data['kategoria_nev'] = $kategoria_nev;
+        
+        $this->load->view('header');
+        $this->load->view('hirdetes_feladas', $data);
+        $this->load->view('footer');
+    }
+
+
+}
+
+/* End of file Hirdetes_feladas.php */
+
+
