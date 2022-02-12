bf36b5dfdfaee111f30e3d86b78e6e14befc929d
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-02-12 18:53:12 +0100

    Konkrét hirdetések egyedi adatlap felületének elkészítése

---
+++ application/views/hirdetes.php
@@ -0,0 +1,47 @@
+<div class="row">
+    <div class="col-sm-4">
+        <img class="img-fluid" src="<?php echo base_url(); ?>images/profilkep_1.jpg" alt="hirdeto_profilkepe">
+    </div>
+    <div class="col-sm-8">
+        <table style="width:100%">
+            <tr>
+                <td>Teljes név:</td>
+                <td>Kiss Anita</td>
+            </tr>
+            <tr>
+                <td>Település:</td>
+                <td>Szentendre</td>
+            <tr>
+                <td>Cím:</td>
+                <td>2001, Kossuth Lajos utca 7. I./12.</td>
+            </tr>
+            <tr>
+                <td>Telefonszám:</td>
+                <td>06 30 654 34 26</td>
+            </tr>
+            <tr>
+                <td>Másodlagos telefonszám:</td>
+                <td>06 20 897 12 56</td>
+            </tr>
+            <tr>
+                <td>Időpont:</td>
+                <td>2022.02.25. 15:00</td>
+            </tr>
+            <tr>
+                <td>Kategória:</td>
+                <td>Kutyasétáltatás</td>
+            </tr>
+        </table>
+    </div> 
+</div>
+<div class="row">
+    <div class="col-sm-12">
+        <p>Leírás:</p>
+        <p style="text-align: justify;">Ez a hirdetéshez tartozó leírás teljes hosszában történő megjelenítésére szolgál. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+    </div>
+</div>
+<button class="btn btn-warning">Jelentkezem</button>
+<br>
+  
+
+
