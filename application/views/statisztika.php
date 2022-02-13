61fe58c35f3b6bebcd76f2f2d1ed91e97359c4fe
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-02-13 17:32:48 +0100

    Statisztika és ranglista menüpont nézetének létrehozása és kialakítása

---
+++ application/views/statisztika.php
@@ -0,0 +1,62 @@
+<div class="row">
+    <div class="col-sm-6">
+        <h5 style="text-align: center;">Ranglista - Helyezettek listája</h5>
+        <p style="text-align: center;">1.</p>
+        <div class="row">
+            <div class="col-sm-6">
+                <img style="height: 200px; width: 300px; display: block; margin-left: auto; margin-right: auto;" class="img-fluid" src="<?php echo base_url(); ?>images/dij_1.jpg" alt="dij_1">
+            </div>
+            <div class="col-sm-6">
+                <img style="height: 200px; width: 300px; display: block; margin-left: auto; margin-right: auto;" class="img-fluid" src="<?php echo base_url(); ?>images/helyezett_1.jpg" alt="helyezett_1">
+            </div>
+        </div>
+        <p style="text-align: center;">2.</p>
+        <div class="row">
+            <div class="col-sm-6">
+                <img style="height: 200px; width: 300px; display: block; margin-left: auto; margin-right: auto;" class="img-fluid" src="<?php echo base_url(); ?>images/dij_2.jpg" alt="dij_1">
+            </div>
+            <div class="col-sm-6">
+                <img style="height: 200px; width: 300px; display: block; margin-left: auto; margin-right: auto;" class="img-fluid" src="<?php echo base_url(); ?>images/helyezett_2.jpg" alt="helyezett_1">
+            </div>
+        </div>
+        <p style="text-align: center;">3.</p>
+        <div class="row">
+            <div class="col-sm-6">
+                <img style="height: 200px; width: 300px; display: block; margin-left: auto; margin-right: auto;" class="img-fluid" src="<?php echo base_url(); ?>images/dij_3.jpg" alt="dij_1">
+            </div>
+            <div class="col-sm-6">
+                <img style="height: 200px; width: 300px; display: block; margin-left: auto; margin-right: auto;" class="img-fluid" src="<?php echo base_url(); ?>images/helyezett_3.jpg" alt="helyezett_1">
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+    <h5 style="text-align: center;">Statisztika - Hirdetések kategóriája szerint</h5>
+    <p style="text-align: center;">1.</p>
+        <div class="row">
+            <div class="col-sm-6">
+                <img style="height: 200px; width: 300px; display: block; margin-left: auto; margin-right: auto;" class="img-fluid" src="<?php echo base_url(); ?>images/kategoria_id_1.jpg" alt="kategoria_1">
+            </div>
+            <div class="col-sm-6">
+                <p style="text-align: center; padding: 70px; font-weight: bold; font-size: x-large;">10 %</p>
+            </div>
+        </div>
+        <p style="text-align: center;">2.</p>
+        <div class="row">
+            <div class="col-sm-6">
+                <img style="height: 200px; width: 300px; display: block; margin-left: auto; margin-right: auto;" class="img-fluid" src="<?php echo base_url(); ?>images/kategoria_id_2.jpg" alt="kategoria_2">
+            </div>
+            <div class="col-sm-6">
+                <p style="text-align: center; padding: 70px; font-weight: bold; font-size: x-large;">20 %</p>
+            </div>
+        </div>
+        <p style="text-align: center;">3.</p>
+        <div class="row">
+            <div class="col-sm-6">
+                <img style="height: 200px; width: 300px; display: block; margin-left: auto; margin-right: auto;" class="img-fluid" src="<?php echo base_url(); ?>images/kategoria_id_3.jpg" alt="kategoria_3">
+            </div>
+            <div class="col-sm-6">
+                <p style="text-align: center; padding: 70px; font-weight: bold; font-size: x-large;">16 %</p>
+            </div>
+        </div>
+    </div>
+</div>
