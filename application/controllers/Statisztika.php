d7e0071d6ed74f768d5230d92934cdb9e31e0f18
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-02-13 17:31:54 +0100

    Statisztika és ranglista aloldal megtekintéséhez controller létrehozása

---
+++ application/controllers/Statisztika.php
@@ -0,0 +1,24 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Statisztika extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+
+    public function ranglista_megtekintes(){
+
+        $this->load->view('header');
+        $this->load->view('statisztika');
+        $this->load->view('footer');
+    }
+
+
+}
+
+/* End of file Statisztika.php */
