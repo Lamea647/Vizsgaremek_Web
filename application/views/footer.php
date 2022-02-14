0c55aa52f93ebd9a9aef329da3a4f4ccaaccc193
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-02-14 17:22:21 +0100

    Lábléc formázása, footer pozíciója miatti változtatás

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,11 +1,12 @@
-    <footer>
-        <div style="text-align: center;">
-            <h5>Önkéntes Portál Nonprofit Kft.</h5><br>  
-            <p><i class="fas fa-phone"></i> +36 1 200 2020</p>
-            <p><i class="fas fa-envelope"></i> onkentesportal@portal.hu</p>
-            <p><i class="fas fa-map-marked"></i> 1111 Budapest, Budafoki út 59.</p>
-        </div>
-    </footer>
+</div>
+<footer id="lablec">
+    <div class="col" style="text-align: center; background-color: blanchedalmond; padding-top: 30px; padding-bottom: 15px;">
+        <h5>Önkéntes Portál Nonprofit Kft.</h5><br>
+        <p><i class="fas fa-phone"></i> +36 1 200 2020</p>
+        <p><i class="fas fa-envelope"></i> onkentesportal@portal.hu</p>
+        <p><i class="fas fa-map-marked"></i> 1111 Budapest, Budafoki út 59.</p>
+    </div>
+</footer>
 </div>
 </body>
 </html>
