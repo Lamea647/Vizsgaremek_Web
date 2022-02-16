b812e9413bed2dd14dc9e5918f388d549eb06160
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-02-16 21:47:40 +0100

    Regisztrációs űrlap beviteli mező neveinek módosítása

--- application/views/regisztracio.php
+++ application/views/regisztracio.php
@@ -1,37 +1,37 @@
 <form method="post" enctype="multipart/form-data">
       <div class="form-group">
-            <label for="nev">Teljes név</label>
+            <label for="nev">Teljes név:</label>
             <input type="text" class="form-control" id="nev" name="nev" maxlength="100" required value="<?php echo isset($nev)?$nev:""; ?>">
       </div>
       <div class="form-group">
-            <label for="felhnev">Felhasználónév</label>
+            <label for="felhnev">Felhasználónév:</label>
             <input type="text" class="form-control" id="felhnev" name="felhnev" maxlength="100" required value="<?php echo isset($felhnev)?$felhnev:""; ?>">
       </div>
       <div class="form-group">
-            <label for="szuldatum">Születési dátum</label>
+            <label for="szuldatum">Születési dátum:</label>
             <input type="date" class="form-control" id="szuldatum" name="szuldatum" required value="<?php echo isset($szuldatum)?$szuldatum:""; ?>">
       </div>
       <div class="form-group">
-            <label for="telszam">Telefonszám</label>
+            <label for="telszam">Telefonszám:</label>
             <input type="tel" class="form-control" id="telszam" name="telszam" required value="<?php echo isset($telszam)?$telszam:""; ?>">
       </div>
       <div class="form-group">
-            <label for="email">E-mail cím</label>
+            <label for="email">E-mail cím:</label>
             <input type="email" class="form-control" id="email" name="email" maxlength="100" required value="<?php echo isset($email)?$email:""; ?>">
       </div>
       <div class="form-group">
-            <label for="telepules_id">Település</label>
+            <label for="telepules_id">Település:</label>
             <select class="form-control" id="telepules_id" name="telepules_id" required>
             <?php for ($x = 0; $x < $szam; $x++) {?>
             <option value="<?php echo $x+1; ?>"><?php echo $telepules[$x]['telepules']; ?></option> <?php } ?>
             </select> 
       </div>
       <div class="form-group">
-            <label for="cim">Cím</label>
+            <label for="cim">Cím:</label>
             <input type="text" class="form-control" id="cim" name="cim" maxlength="100" required placeholder="irányítószám, utca, házszám, emelet, ajtó"value="<?php echo isset($cim)?$cim:""; ?>">
       </div>
       <div class="form-group">
-            <label for="okmanyszam">Feltöltött okmány száma</label>
+            <label for="okmanyszam">Feltöltött okmány száma:</label>
             <input type="text" class="form-control" id="okmanyszam" name="okmanyszam" maxlength="100" required value="<?php echo isset($okmanyszam)?$okmanyszam:""; ?>">
       </div>
       <div class="form-group">
@@ -43,11 +43,11 @@
         <input accept=".jpg,.jpeg,.png,.bmp" type="file" class="form-control-file" id="profilkep" name="profilkep" required>
       </div>
       <div class="form-group">
-            <label for="jelszo">Jelszó</label>
+            <label for="jelszo">Jelszó:</label>
             <input type="password" class="form-control" id="jelszo" name="jelszo" maxlength="100" required>
       </div>
       <div class="form-group">
-            <label for="jelszoujra">Jelszó megerősítése</label>
+            <label for="jelszoujra">Jelszó megerősítése:</label>
             <input type="password" class="form-control" id="jelszoujra" name="jelszoujra" maxlength="100" required>
       </div>
       <div class="form-group">
