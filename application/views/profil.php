1c5a91a9a58b1cce1e6d0e583a0763e50f91c775
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-03-02 21:45:54 +0100

    Képcsere és adatcsere

--- application/views/profil.php
+++ application/views/profil.php
@@ -1,16 +1,16 @@
 <div class="row">
     <div class="col-sm-4">
-        <img class="img-fluid" src="<?php echo base_url(); ?>images/profilkep_1.jpg" alt="felhasznalo_profilkepe">
+        <img class="img-fluid" src="<?php echo base_url(); ?>uploads/tesztelek_profil_2022_03_02_13_30_00.jpg" alt="felhasznalo_profilkepe">
     </div>
     <div class="col-sm-8">
         <table style="width:30%">
             <tr>
                 <td>Teljes név:</td>
-                <td>Kiss Anita</td>
+                <td>Teszt Elek</td>
             </tr>
             <tr>
                 <td>Felhasználónév:</td>
-                <td>kissa</td>
+                <td>tesztelek</td>
             </tr>
         </table>
     <button class="btn btn-warning">Profil módosítása</button>
@@ -49,43 +49,43 @@
 </div><br>
 <p style="font-weight: bold;">Díjaim:</p>
 <div class="row">
-    <div class="col-sm-2">
-        <img style="width: 100px; height: 100px;" class="img-fluid" src="<?php echo base_url(); ?>images/kategoria_id_1.jpg" alt="kategoria_kepe">
+    <div class="col-sm-3 col-lg-2">
+        <img style="width: 200px; height: 150px;" class="img-thumbnail" src="<?php echo base_url(); ?>images/kutyasetaltatas.jpg" alt="kategoria_kepe">
     </div>
-    <div class="col-sm-2">
-        <img style="width: 100px; height: 100px;" class="img-fluid" src="<?php echo base_url(); ?>images/kategoria_id_2.jpg" alt="kategoria_kepe">
+    <div class="col-sm-3 col-lg-2">
+        <img style="width: 200px; height: 150px;" class="img-thumbnail" src="<?php echo base_url(); ?>images/bevasarlas.jpg" alt="kategoria_kepe">
     </div>
-    <div class="col-sm-2">
-        <img style="width: 100px; height: 100px;" class="img-fluid" src="<?php echo base_url(); ?>images/kategoria_id_3.jpg" alt="kategoria_kepe">
+    <div class="col-sm-3 col-lg-2">
+        <img style="width: 200px; height: 150px;" class="img-thumbnail" src="<?php echo base_url(); ?>images/takaritas.jpg" alt="kategoria_kepe">
     </div>
 </div>
 <p style="font-weight: bold;">Hirdetések, amikre jelentkeztem:</p>
 <div class="row">
-    <div class="col-sm-2">
-        <div class="card" style="width:100px">
-            <img class="card-img-bottom" src="<?php echo base_url(); ?>images/kategoria_id_2.jpg" alt="kategoria_kepe" style="width:100%">
+    <div class="col-sm-12 col-md-4 col-lg-3">
+        <div class="card" style="width:100%">
+            <img class="card-img-bottom" src="<?php echo base_url(); ?>images/bevasarlas.jpg" alt="kategoria_kepe" style="width:100%">
             <a href="#" class="btn btn-warning">Tovább a hirdetésre</a>
         </div>
     </div>
-    <div class="col-sm-2">
-        <div class="card" style="width:100px">
-            <img class="card-img-bottom" src="<?php echo base_url(); ?>images/kategoria_id_1.jpg" alt="kategoria_kepe" style="width:100%">
+    <div class="col-sm-12 col-md-4 col-lg-3">
+        <div class="card" style="width:100%">
+            <img class="card-img-bottom" src="<?php echo base_url(); ?>images/kutyasetaltatas.jpg" alt="kategoria_kepe" style="width:100%">
             <a href="#" class="btn btn-warning">Tovább a hirdetésre</a>
         </div>
     </div>
 </div><br>
 <p style="font-weight: bold;">Saját hirdetéseim:</p>
 <div class="row">
-    <div class="col-sm-2">
-        <div class="card" style="width:100px;">
-            <img class="card-img-bottom" src="<?php echo base_url(); ?>images/kategoria_id_2.jpg" alt="kategoria_kepe" style="width:100%;">
+    <div class="col-sm-12 col-md-4 col-lg-3">
+        <div class="card" style="width:100%;">
+            <img class="card-img-bottom" src="<?php echo base_url(); ?>images/bevasarlas.jpg" alt="kategoria_kepe" style="width:100%;">
             <a href="#" class="btn btn-warning">Tovább a hirdetésre</a>
             <a href="#" class="btn btn-danger">Törlés</a>
         </div>
     </div>
-    <div class="col-sm-2">
-        <div class="card" style="width:100px">
-            <img class="card-img-bottom" src="<?php echo base_url(); ?>images/kategoria_id_3.jpg" alt="kategoria_kepe" style="width:100%">
+    <div class="col-sm-12 col-md-4 col-lg-3">
+        <div class="card" style="width:100%">
+            <img class="card-img-bottom" src="<?php echo base_url(); ?>images/takaritas.jpg" alt="kategoria_kepe" style="width:100%">
             <a href="#" class="btn btn-warning">Tovább a hirdetésre</a>
             <a id="torlesgomb" onclick="hirdetesTorles()" href="#" class="btn btn-danger">Törlés</a>
         </div>
