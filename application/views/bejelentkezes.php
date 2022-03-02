c4e9f17fc24d8f77da84baf79325057703b3ea11
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-03-02 21:37:36 +0100

    Lemaradt kettőspontok pótlása a jelszó és felhasználónév mezők esetében

--- application/views/bejelentkezes.php
+++ application/views/bejelentkezes.php
@@ -1,11 +1,11 @@
 <div class="col-lg-6 mx-auto" style="margin-top: 5%;">
 <form action="<?php echo base_url(); ?>bejelentkezes" method="post">
     <div class="form-group">
-        <label for="felhnev">Felhasználónév</label>
+        <label for="felhnev">Felhasználónév:</label>
         <input type="text" class="form-control" id="felhnev" name="felhnev" required>
     </div>
     <div class="form-group">
-        <label for="jelszo">Jelszó</label>
+        <label for="jelszo">Jelszó:</label>
         <input type="password" class="form-control" id="jelszo" name="jelszo" required>
     </div>
 <button type="submit" class="btn btn-warning" name="belepes" value="true">Bejelentkezés</button>
