3e2c1bb815a5c9f7fab691d18a1f5f370c6a2e8e
Dénes Evelin <denes.evelin@gmail.com>
2022-03-07 19:04:53 +0100

    User_model létrehozása + Api/User index_get metódusainak elkészítése

---
+++ application/controllers/api/User.php
@@ -0,0 +1,47 @@
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require_once APPPATH. 'libraries/REST_Controller.php';
+
+class User extends REST_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('user_model');
+    }
+
+    public function index_get($id = 0)
+    {   
+        $adatok = [];
+        $error = false;
+        $message = "Felhasználó sikeresen lekérdezve.";
+        $response_code = REST_Controller::HTTP_OK;
+        if ($id == 0) {
+            $adatok = $this->user_model->get_all();
+        } else{
+            $adatok = $this->user_model->get_by_id($id);
+            if (is_null($adatok)) {
+                $error = true;
+                $message = "A megadott azonosítóval nem található felhasználó.";
+                $adatok = [];
+                $response_code = REST_Controller::HTTP_NOT_FOUND;
+            } else{
+                $adatok = [$adatok];
+            }
+        }
+        $data = [
+            'adatok' => $adatok,
+            'error' => $error,
+            'message' => $message,
+        ];
+        $this->response($data, $response_code);
+    }
+
+    
+
+    
+
+    
+
+}
---
+++ application/models/User_model.php
@@ -0,0 +1,32 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User_model extends CI_Model {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function get_all()
+    {
+        $this->db->order_by('user_id', 'desc');
+        return $this->db->get('user')->result_array();
+    }
+
+    public function get_by_id($id)
+    {
+        $this->db->where('user_id', $id);
+        return $this->db->get('user')->row_array();
+    }
+
+    
+        
+}
+
+/* End of file ModelName.php */
+
+
+?>
