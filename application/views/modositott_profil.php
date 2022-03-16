2c000a991989f4957425ea84ceaddc7654ec2878
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-03-16 20:29:04 +0100

    Jelszó módosításnak külön form készítés. Aktív felhasználó adatainak betöltése a formok mezőibe.

--- application/views/modositott_profil.php
+++ application/views/modositott_profil.php
@@ -2,26 +2,30 @@
 <div class="container"><h6 style="font-weight: bold; text-align: center;">Profil módosítása (jóváhagyás nélkül, azonnal életbe lépnek):</h6></div>
 <div class="container">
     <div class="form-group">
-        <label for="telepules_id">Település:</label>
-        <select class="form-control" id="telepules_id" name="telepules_id" required>
-        <option value="telepules_id"><?php echo $telepules['telepules']; ?></option>
-        </select> 
+            <label for="telepules_id">Település:</label>
+            <select class="form-control" id="telepules_id" name="telepules_id" required>
+            <?php for ($x = 0; $x < $szam; $x++):?>
+                <option value="
+                <?php if($x+1 == $_SESSION['user']['telepules_id']):?>
+                    <?php echo $x+1; ?>" selected>
+                <?php else: echo $x+1; ?>"><?php endif ?><?php echo $telepules[$x]['telepules']; ?><?php endfor ?></option> 
+            </select> 
     </div>
     <div class="form-group">
             <label for="cim">Cím:</label>
-            <input type="text" class="form-control" id="cim" name="cim" maxlength="100" required value=<?php echo $_SESSION['user']['cim']; ?>>
+            <input type="text" class="form-control" id="cim" name="cim" maxlength="100" required value="<?php echo $_SESSION['user']['cim']; ?>">
     </div>
     <div class="form-group">
-        <label for="email">E-mail cím:</label>
-        <input type="email" class="form-control" id="email" name="email" maxlength="100" required value=<?php echo $_SESSION['user']['email']; ?>>
+            <label for="email">E-mail cím:</label>
+            <input type="email" class="form-control" id="email" name="email" maxlength="100" required value="<?php echo $_SESSION['user']['email']; ?>">
     </div>
     <div class="form-group">
             <label for="telszam">Telefonszám:</label>
-            <input type="tel" class="form-control" id="telszam" name="telszam" required value=<?php echo $_SESSION['user']['telszam']; ?>>
+            <input type="tel" class="form-control" id="telszam" name="telszam" required value="<?php echo $_SESSION['user']['telszam']; ?>">
     </div>
     <div class="form-group">
-    <label for="profilkep">Új profilkép feltöltése:</label>
-    <input accept=".jpg,.jpeg,.png,.bmp" type="file" class="form-control-file" id="profilkep" name="profilkep" required>
+            <label for="profilkep">Új profilkép feltöltése:</label>
+            <input accept=".jpg,.jpeg,.png,.bmp" type="file" class="form-control-file" id="profilkep" name="profilkep" required>
     </div>
     <button type="submit" class="btn btn-warning">Profil módosítása</button>
 </div><br>
@@ -40,17 +44,22 @@
             <label for="jelszoujra">Új jelszó újra:</label>
             <input type="password" class="form-control" id="jelszoujra" name="jelszoujra" maxlength="100" required>
       </div>
+      <button type="submit" class="btn btn-warning">Profil módosítása</button>
+</div>
+
+<div class="container"><h6 style="font-weight: bold; text-align: center;">Profil módosítása (jóváhagyás szükséges az adatok módosítását követően):</h6></div>
+<div class="container">
       <div class="form-group">
             <label for="nev">Teljes név</label>
-            <input type="text" class="form-control" id="nev" name="nev" maxlength="100" required value=<?php echo $_SESSION['user']['nev']; ?>>
+            <input type="text" class="form-control" id="nev" name="nev" maxlength="100" required value="<?php echo $_SESSION['user']['nev']; ?>">
       </div>
       <div class="form-group">
             <label for="okmanyszam">Feltöltött új okmány száma:</label>
-            <input type="text" class="form-control" id="okmanyszam" name="okmanyszam" maxlength="100" required value="">
+            <input type="text" class="form-control" id="okmanyszam" name="okmanyszam" maxlength="100" required value="<?php echo $_SESSION['user']['okmanyszam']; ?>">
       </div>
       <div class="form-group">
-        <label for="okmanykep">Új okmánykép feltöltése:</label>
-        <input accept=".jpg,.jpeg,.png,.bmp" type="file" class="form-control-file" id="okmanykep" name="okmanykep" required>
+            <label for="okmanykep">Új okmánykép feltöltése:</label>
+            <input accept=".jpg,.jpeg,.png,.bmp" type="file" class="form-control-file" id="okmanykep" name="okmanykep" required>
       </div>
     <button type="submit" class="btn btn-warning">Profil módosítása</button>
 </div><br>
