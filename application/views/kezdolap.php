306980d909407908179d0db2afe4828410e5144c
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-07-25 21:28:19 +0200

    Bemutatkozás és Szabályzat vázlatszöveg hozzáadása

--- application/views/kezdolap.php
+++ application/views/kezdolap.php
@@ -2,13 +2,16 @@
     <div class="row">
         <div class="col-md-6">
             <p style="text-align: justify; text-indent: 20px;">
-            Curabitur a orci sed nisi rutrum laoreet. Vestibulum et mattis nisl. Ut ac eros maximus quam interdum lobortis eu at augue. 
-            Aliquam erat volutpat. Suspendisse malesuada molestie tellus, at porta ex imperdiet ut. Fusce pellentesque facilisis varius. Sed viverra facilisis dolor, at mattis purus varius et. Nam dapibus congue pretium. Suspendisse potenti.
-            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse tempus tortor orci, nec elementum neque porta semper. 
-            Donec fringilla, quam a finibus ullamcorper, ex enim tincidunt ex, vestibulum pharetra libero justo vitae mi. Mauris rhoncus sem non orci ultrices sodales. 
-            Integer augue dolor, congue quis ex vel, pharetra semper justo. Quisque nulla lorem, condimentum a ultrices sit amet, tempor quis nibh. 
-            Morbi sagittis feugiat metus sit amet accumsan. Mauris feugiat, velit venenatis rutrum suscipit, sem ligula faucibus dolor, vel semper ipsum risus sit amet quam. 
-            Maecenas at hendrerit turpis, quis vehicula leo. Nunc pellentesque nunc quis consequat sollicitudin. 
+            Üdvözöljük az Önkéntes Portál Nonprofit Kft. honlapján. 
+            Immáron 2 éve azon munkálkodunk, hogy népszerűsítsük az önkéntes munkát, ezen keresztül pedig erősítsük a társadalmi felelősségvállalást, 
+            a szociális érzékenységet és az önzetlen segítségnyújtást. Weboldalunkon és mobil applikációnkon keresztül könnyedén egymásra találhatnak 
+            a segítő szándékú és a segítségre szoruló emberek. Hiszünk abban, hogy kis változásokkal is nagy eredményeket lehet elérni, és egy jobb és 
+            élhetőbb világ felé haladhatunk. Szlogenünk, az ,,Önként Önért” magában foglalja alapelveinket, miszerint önkéntes segítségnyújtással, 
+            embertársainkra való odafigyeléssel sokat tehetünk egymásért, és jobb emberekké válhatunk. Örömünkre szolgál, ha csatlakozik csapatunkhoz 
+            és célkitűzésünket elősegítve minél több emberhez eljuttatja elérhetőségeinket.
+
+            <p style="text-align: right;">
+            Köszönettel az alapítótagok:<br>Dénes Evelin és Gulácsi-Szabó Tímea</p>
             </p></div>
         <div class="col-md-6"><img src="<?php echo base_url(); ?>images/bem1.jpg" alt="Bemutatkozó szöveghez tartozó kép." style="width: 400px; height: 300px;" class="img-fluid mx-auto d-block" title="Bemutatkozás"></div>
     </div><br>
@@ -16,13 +19,13 @@
     <div class="row">
         <div class="col-md-6">
             <p style="text-align: justify; text-indent: 20px;">
-            Curabitur a orci sed nisi rutrum laoreet. Vestibulum et mattis nisl. Ut ac eros maximus quam interdum lobortis eu at augue. 
-            Aliquam erat volutpat. Suspendisse malesuada molestie tellus, at porta ex imperdiet ut. Fusce pellentesque facilisis varius. Sed viverra facilisis dolor, at mattis purus varius et. Nam dapibus congue pretium. Suspendisse potenti.
-            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse tempus tortor orci, nec elementum neque porta semper. 
-            Donec fringilla, quam a finibus ullamcorper, ex enim tincidunt ex, vestibulum pharetra libero justo vitae mi. Mauris rhoncus sem non orci ultrices sodales. 
-            Integer augue dolor, congue quis ex vel, pharetra semper justo. Quisque nulla lorem, condimentum a ultrices sit amet, tempor quis nibh. 
-            Morbi sagittis feugiat metus sit amet accumsan. Mauris feugiat, velit venenatis rutrum suscipit, sem ligula faucibus dolor, vel semper ipsum risus sit amet quam. 
-            Maecenas at hendrerit turpis, quis vehicula leo. Nunc pellentesque nunc quis consequat sollicitudin. 
+            Cégünk profiljából adódóan felhasználóink között nagyon fontos szerepet játszik az egymás iránti bizalom, így kizárólag valós személyes 
+            adatok megadásával történő regisztrációt követően láthatóak az önkéntes munkalehetőségek. Minden esetben szükséges a személyes adatok 
+            megadása mellett profilképet és személyes okmányt is feltölteni, valamint okmányszámot megadni. Regisztrációt követően a feltöltött adatok
+            és képek hitelesítési folyamaton mennek keresztül. Az adatok jóváhagyását követően a regisztrált felhasználó belépési jogot szerez, melyet
+            egyes személyes adatok módosítása esetén bizonyos esetekben ideiglenesen visszavonunk az újabb ellenőrzési folyamat lefuttatásáig. 
+            Regisztráció esetén kötelező elfogadni a Felhasználói feltételeket és az Adatvédelmi irányelveket, melyek a felhasználók érdekeit, 
+            személyes adatainak védelmét szolgálják.
             </p>
         </div>
         <div class="col-md-6"><img src="<?php echo base_url(); ?>images/bem2.jpg" alt="Szabályzat szöveghez tartozó kép." style="width: 400px; height: 300px;" class="img-fluid mx-auto d-block" title="Szabályzat"></div>
