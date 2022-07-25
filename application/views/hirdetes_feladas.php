ad43466536011eb2845df105e25ba66e51e3e291
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-07-25 21:30:22 +0200

    Hirdetés feladásához szükséges form szerkesztése

--- application/views/hirdetes_feladas.php
+++ application/views/hirdetes_feladas.php
@@ -1,5 +1,6 @@
 <div class="container"><h5 style="font-weight: bold; text-align: center;">Új hirdetés adatai:</h5></div>
     <div class="container col-lg-8">
+    <form action="<?php echo base_url(); ?>hirdetes_feladas" method="post">
         <div class="form-group">
             <label for="kezdo_idopont">Kezdő időpont:</label>
             <input type="datetime-local" class="form-control" id="kezdo_idopont" name="kezdo_idopont">
@@ -30,16 +31,17 @@
             </select> 
         </div>
         <div class="form-group">
-                <label for="cim">Cím:</label>
-                <input type="text" class="form-control" id="cim" name="cim" maxlength="100" required value="<?php echo $_SESSION['user']['cim']; ?>">
+                <label for="hirdetes_cim">Cím:</label>
+                <input type="text" class="form-control" id="hirdetes_cim" name="hirdetes_cim" maxlength="100" required value="<?php echo $_SESSION['user']['cim']; ?>">
         </div>
         <div class="form-group">
-                <label for="telszam">Telefonszám</label>
-                <input type="tel" class="form-control" id="telszam" name="telszam" required value="<?php echo $_SESSION['user']['telszam']; ?>">
+                <label for="telszam">Telefonszám:</label>
+                <input type="tel" class="form-control" id="telszam" name="telszam" disabled value="<?php echo $_SESSION['user']['telszam']; ?>">
         </div>
         <div class="form-group">
-                <label for="telszam_masik">Másodlagos telefonszám</label>
-                <input type="telszam_masik" class="form-control" id="telszam_masik" name="telszam_masik" required placeholder="Opcionálisan megadható másik telefonszám">
+                <label for="telszam_2">Másodlagos telefonszám:</label>
+                <input type="telszam_2" class="form-control" id="telszam_2" name="telszam_2" placeholder="Opcionálisan megadható másik telefonszám">
         </div>
-        <button type="submit" class="btn btn-warning">Hirdetés feladása</button>
+        <button type="submit" class="btn btn-warning" name="hirdetes_feladas" value="true">Hirdetés feladása</button>
+        </form>
     </div><br>
