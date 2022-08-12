67572e8885939f0ea766ab48f6ae16f8788f439a
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-08-12 14:28:55 +0200

    Formázás

--- application/models/Regisztracio_model.php
+++ application/models/Regisztracio_model.php
@@ -4,32 +4,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Regisztracio_model extends CI_Model {
 
-    public function __construct() {
+    public function __construct(){
         parent::__construct();
         $this->load->database();
     }
 
-    public function telepules_lista()
-    {
-    $this->db->select("telepules");
-    $query = $this->db->get("telepules");
-    return $result = $query->result_array();
+    //településnevek lekérdezése
+    public function telepules_lista(){
+        $this->db->select('telepules');
+        $query = $this->db->get('telepules');
+        return $result = $query->result_array();
     }
 
-//település tábla rekordjainak megszámlása és az eredmény lekérdezése
-    public function telepulesekSzama()
-    {
+    //település tábla rekordjainak megszámlása és az eredmény lekérdezése
+    public function telepulesekSzama(){
         return $this->db->count_all('telepules');
     }
 
-    public function insert($data)
-    {
+    //felhasználó adatainak rögzítése user táblába
+    public function insert($data){
         $this->db->insert('user', $data);
     }
 
-
-    public function kereses_felhnev_alapjan($felhnev)
-    {
+    //felhasználónév alapján adott felhasználó adatainak lekérdezése
+    public function kereses_felhnev_alapjan($felhnev){
         $this->db->where('felhnev', $felhnev);
         return $this->db->get('user')->row_array();
     }
