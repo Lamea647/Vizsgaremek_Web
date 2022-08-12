f13676112b1829dfe278bfeb425e057c710f03f0
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-08-12 14:28:08 +0200

    Formázás

--- application/controllers/Statisztika.php
+++ application/controllers/Statisztika.php
@@ -5,15 +5,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Statisztika extends CI_Controller {
 
-    public function __construct()
-    {
+    public function __construct(){
         parent::__construct();
         $this->load->helper('url');
         $this->load->library('session');
     }
 
     public function ranglista_megtekintes(){
-
         $this->load->view('header', ['oldal' => 'ranglista']);
         $this->load->view('statisztika');
         $this->load->view('footer');
