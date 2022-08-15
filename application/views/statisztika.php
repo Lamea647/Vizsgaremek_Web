924e45cb8a7f5f7b60b44dd754594e056bca77aa
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-08-15 22:02:27 +0200

    Hirdetésekre vonatkozó statisztikai adatok megjelenítésének tesztelése

--- application/views/statisztika.php
+++ application/views/statisztika.php
@@ -59,4 +59,50 @@
             </div>
         </div>
     </div>
-</div>
+</div> 
+
+
+<?php //Statisztikai adatok megjelenítésének tesztelése
+foreach($adatok as $adat){
+    echo $adat['kategoria_id'] . ".kategória - " . $adat['COUNT(hirdetes_id)'] . " db <br>";
+    echo $adat['kategoria_id'] . ".kategória - " . $adat['COUNT(hirdetes_id)']/$hirdetes_szam*100 . " % <br>";
+}
+?>
+
+<?php
+for ($i=0; $i < 12 ; $i++):?>
+    <?php for ($j=0; $j < 2 ; $j++):?>
+        <?php if($adatok[$j]['kategoria_id'] == $i+1):?>
+            <p style="text-align: center;"><?php echo $adatok[$j]['kategoria_id'];?></p>
+            <div class="row">
+                <div class="col-sm-6">
+                    <img style="height: 200px; width: 300px; display: block; margin-left: auto; margin-right: auto;" class="img-fluid" src="<?php echo base_url(); ?>images/<?php echo $kategoriaAdatok[$j+1]['kategoria_kep']; ?>" alt="kategoria_2">
+                </div>
+                <div class="col-sm-6">
+                    <p style="text-align: center; padding: 70px; font-weight: bold; font-size: x-large;"><?php echo $adatok[$j]['COUNT(hirdetes_id)']/$hirdetes_szam*100; ?>%</p>
+                </div>
+            </div> 
+        <?php endif; ?>
+    <?php endfor; ?>
+            <p style="text-align: center;"><?php echo $kategoriaAdatok[$i]['kategoria_id']?></p>
+            <div class="row">
+                <div class="col-sm-6">
+                    <img style="height: 200px; width: 300px; display: block; margin-left: auto; margin-right: auto;" class="img-fluid" src="<?php echo base_url(); ?>images/<?php echo $kategoriaAdatok[$i]['kategoria_kep']; ?>" alt="kategoria_2">
+                </div>
+                <div class="col-sm-6">
+                    <p style="text-align: center; padding: 70px; font-weight: bold; font-size: x-large;">0 %</p>
+                </div>
+            </div>   
+<?php endfor; ?>
+
+
+<?php var_dump($kategoriaAdatok);?>
+<?php echo $kategoriaAdatok[0]['kategoria_id'];
+echo $kategoriaAdatok[0]['kategoria_nev'];
+echo $kategoriaAdatok[1]['kategoria_kep'];
+echo $adatok[0]['kategoria_id'];
+echo $adatok[1]['kategoria_id'];
+
+
+
+
