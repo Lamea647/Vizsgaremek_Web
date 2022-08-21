47e28b330a38b016a288c8084f5e52b5d728870d
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-08-21 14:33:07 +0200

    Hirdetés feladás frontend validálás metódusainak elkészítése, user adat visszatöltés visszavonása

--- application/views/hirdetes_feladas.php
+++ application/views/hirdetes_feladas.php
@@ -3,45 +3,70 @@
     <form action="<?php echo base_url(); ?>hirdetes_feladas" method="post">
         <div class="form-group">
             <label for="kezdo_idopont">Kezdő időpont:</label>
-            <input type="datetime-local" class="form-control" id="kezdo_idopont" name="kezdo_idopont">
+            <input type="datetime-local" class="form-control" id="kezdo_idopont" name="kezdo_idopont" required>
         </div> 
         <div class="form-group">
             <label for="zaro_idopont">Záró időpont:</label>
-            <input type="datetime-local" class="form-control" id="zaro_idopont" name="zaro_idopont">
+            <input type="datetime-local" class="form-control" id="zaro_idopont" name="zaro_idopont" required>
         </div> 
         <div class="form-group">
             <p>Kategória:</p>
-            <select class="form-control" id="kategoria_id" name="kategoria_id">
+            <select class="form-control" id="kategoria_id" name="kategoria_id" required>
             <?php for ($x = 0; $x < $kategoria_szam; $x++) {?>
             <option value="<?php echo $x+1; ?>"><?php echo $kategoria_nev[$x]['kategoria_nev']; ?></option> <?php } ?>
             </select> 
         </div>
         <div class="form-group">
             <label for="leiras">Leírás:</label>
-            <textarea class="form-control" id="leiras" name="leiras" rows="5" cols="10"></textarea>
+            <textarea class="form-control" id="leiras" name="leiras" rows="5" cols="10" required></textarea>
         </div> 
         <div class="form-group">
             <label for="telepules_id">Település:</label>
             <select class="form-control" id="telepules_id" name="telepules_id" required>
-            <?php for ($x = 0; $x < $telepules_szam; $x++):?>
-                <option value="
-                <?php if($x+1 == $_SESSION['user']['telepules_id']):?>
-                    <?php echo $x+1; ?>" selected>
-                <?php else: echo $x+1; ?>"><?php endif ?><?php echo $telepules[$x]['telepules']; ?><?php endfor ?></option> 
+                <?php for ($x = 0; $x < $telepules_szam; $x++) {?>
+                <option value="<?php echo $x+1; ?>"><?php echo $telepules[$x]['telepules']; ?></option> <?php } ?>
             </select> 
         </div>
         <div class="form-group">
                 <label for="hirdetes_cim">Cím:</label>
-                <input type="text" class="form-control" id="hirdetes_cim" name="hirdetes_cim" maxlength="100" required value="<?php echo $_SESSION['user']['cim']; ?>">
+                <input type="text" class="form-control" id="hirdetes_cim" name="hirdetes_cim" maxlength="100" pattern=".{8,100}" required onchange="validalas_cim();">
         </div>
+        <span id="hirdetes_cim_hiba" style="color: red;"></span>
         <div class="form-group">
-                <label for="telszam">Telefonszám:</label>
-                <input type="tel" class="form-control" id="telszam" name="telszam" disabled value="<?php echo $_SESSION['user']['telszam']; ?>">
+                <label for="hirdetes_telszam">Telefonszám:</label>
+                <input type="tel" class="form-control" id="hirdetes_telszam" name="hirdetes_telszam" maxlength="30" pattern=".{7,30}" required onchange="validalas_telszam();">
         </div>
-        <div class="form-group">
-                <label for="telszam_2">Másodlagos telefonszám:</label>
-                <input type="telszam_2" class="form-control" id="telszam_2" name="telszam_2" placeholder="Opcionálisan megadható másik telefonszám">
-        </div>
-        <button type="submit" class="btn btn-warning" name="hirdetes_feladas" value="true">Hirdetés feladása</button>
+        <span id="hirdetes_telszam_hiba" style="color: red;"></span><br>
+        <button type="submit" onclick="validalas();" class="btn btn-warning" name="hirdetes_feladas" value="true">Hirdetés feladása</button>
         </form>
     </div><br>
+
+<script>
+    function validalas(){
+        validalas_cim();
+        validalas_telszam();
+    }
+</script>
+
+<script>
+    function validalas_cim(){
+        let hirdetes_cim = document.getElementById("hirdetes_cim").value;
+        if(hirdetes_cim.length < 8){
+            document.getElementById("hirdetes_cim_hiba").innerHTML = "A megadott cím nem lehet 8 karakternél rövidebb.";
+        }else{
+            document.getElementById("hirdetes_cim_hiba").innerHTML = "";
+        }
+    }
+</script>
+
+<script>
+    function validalas_telszam(){
+        let hirdetes_telszam = document.getElementById("hirdetes_telszam").value;
+        if(hirdetes_telszam.length < 7){
+            document.getElementById("hirdetes_telszam_hiba").innerHTML = "A megadott telefonszám nem lehet 7 karakternél rövidebb.";
+        }else{
+            document.getElementById("hirdetes_telszam_hiba").innerHTML = "";
+        }
+    }
+</script>
+
