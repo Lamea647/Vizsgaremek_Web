66be1ffd9ad2a8251a5c8a0a1cc2d70007578b08
Dénes Evelin <denes.evelin@gmail.com>
2022-08-22 16:58:06 +0200

    hirdető nevProfilkep metódusának elkészítése Hirdetes_modelben a conflict után.

--- application/models/Hirdetes_model.php
+++ application/models/Hirdetes_model.php
@@ -67,6 +67,14 @@ class Hirdetes_model extends CI_Model {
         return $result = $query->row_array();
     }
 
+    public function nevProfilkep($hirdetes_id){
+        $this->db->select('nev, profilkep');
+        $this->db->from('user');
+        $this->db->join('hirdetes', 'hirdetes.hirdeto_id = user.user_id');
+        $this->db->where('hirdetes_id', $hirdetes_id);
+        $query = $this->db->get();
+        return $result = $query->result_array();
+    }
 
 
 
