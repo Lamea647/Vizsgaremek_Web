0df7cff0af9324b735c28845342ef1b12d51bac4
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-08-25 17:25:18 +0200

    Modelhivatkozások frissítése, Profil_megtekintes és profil_modositas_post metódusok átalakítása

--- application/controllers/Profil.php
+++ application/controllers/Profil.php
@@ -9,16 +9,46 @@ class Profil extends CI_Controller {
         $this->load->helper('url');
         $this->load->library('session');
         $this->load->model('telepules_model');
-        $this->load->model('regisztracio_model');
-        $this->load->model('hirdetes_feladas_model');
+        $this->load->model('user_model');
+        $this->load->model('hirdetes_model');
+        $this->load->model('jelentkezes_model');
+
     }
 
     public function profil_megtekintes(){
         $telepules = $this->telepules_model->get_by_id($_SESSION['user']['telepules_id']);
         $data['telepules'] = $telepules;
 
-        $data['kategoriak'] = $this->hirdetes_feladas_model->sajatHirdetesekMegjelenitese($_SESSION['user']['user_id']);
+        //Díjaim részhez (teljesített hirdetések képei)
 
+        $kategoriaKepek = $this->user_model->kategoriaKepekListazasa($_SESSION['user']['user_id']);
+        $data['kategoriaKepek'] = $kategoriaKepek;
+
+        //Hirdetések, amikre jelentkeztem részhez:
+
+        $kategoriaKepekJelentkezesek = $this->hirdetes_model->kategoriaKepekListazasaJelentkezes($_SESSION['user']['user_id']);
+        $data['kategoriaKepekJelentkezesek'] = $kategoriaKepekJelentkezesek;
+
+        $hirdetesIdkJelentkezesek = $this->hirdetes_model->hirdetesekAdataiAmikreJelentkezettAFelhasznalo($_SESSION['user']['user_id']);
+        $data['hirdetesIdkJelentkezesek'] = $hirdetesIdkJelentkezesek;
+
+        //Saját hirdetéseim részhez:
+
+        $sajatHirdetesKategoriaKepek = $this->hirdetes_model->kategoriaKepekListazasaSajatHirdetesek($_SESSION['user']['user_id']);
+        $data['sajatHirdetesKategoriaKepek'] = $sajatHirdetesKategoriaKepek;
+
+        $sajatHirdetesAdatok = $this->hirdetes_model->sajatHirdetesekAdatai($_SESSION['user']['user_id']);
+        $data['sajatHirdetesAdatok'] = $sajatHirdetesAdatok;
+
+        //Jóváhagyásra váró hirdetések részhez:
+
+        $jovahagyasravaroKategoriaKepek = $this->hirdetes_model->kategoriaKepekJovahagyasravaroHirdetesek($_SESSION['user']['user_id']);
+        $data['jovahagyasravaroKategoriaKepek'] = $jovahagyasravaroKategoriaKepek;
+
+        $jovahagyasravaroHirdetesekAdatai = $this->hirdetes_model->jovahagyasravaroHirdetesekAdatai($_SESSION['user']['user_id']);
+        $data['jovahagyasravaroHirdetesekAdatai'] = $jovahagyasravaroHirdetesekAdatai;
+
+        
         $this->load->view('header', ['oldal' => 'profil']);
         $this->load->view('profil', $data);
         $this->load->view('footer');
@@ -28,7 +58,7 @@ class Profil extends CI_Controller {
         $telepules = $this->telepules_model->get_all();
         $data['telepules'] = $telepules;
 
-        $telepules_szam = $this->regisztracio_model->telepulesekSzama();
+        $telepules_szam = $this->telepules_model->telepulesekSzama();
         $data['telepules_szam'] = $telepules_szam;
 
         $this->load->view('header');
@@ -37,10 +67,9 @@ class Profil extends CI_Controller {
     }
 
     //személyes adatok módosítása - 1. form
-    public function adatok_mod($id){
-        $this->load->model('user_model');
+    public function profil_modositas_post($id){
 		$this->load->library('form_validation');
-		$this->form_validation->set_rules('telepules_id', 'Település');
+		$this->form_validation->set_rules('telepules_id', 'Település', 'required');
         $this->form_validation->set_rules('cim', 'Cím', 'trim|required');
         $this->form_validation->set_rules('email', 'E-mail cím', 'trim|required|valid_email|is_unique');
         $this->form_validation->set_rules('telszam', 'Telefonszám', 'trim|required');
@@ -51,47 +80,20 @@ class Profil extends CI_Controller {
 			redirect('profil/profil_modositas');
 		}
 
-        $profilkep_nev = $_FILES['profilkep']['name'];
-        $kiterjesztes1 = pathinfo($profilkep_nev, PATHINFO_EXTENSION);
-        $idopont1 = date("Y_m_d_H_i_s");
-        $felhnev1 = $_SESSION['user']['felhnev'];
-        $fajlnev1 = $felhnev1; //egyelőre felhasználónév
-        $fajlnev1 = preg_replace('/[áàãâä]/ui', 'a', $fajlnev1);
-        $fajlnev1 = preg_replace('/[éèêë]/ui', 'e', $fajlnev1);
-        $fajlnev1 = preg_replace('/[íìîï]/ui', 'i', $fajlnev1);
-        $fajlnev1 = preg_replace('/[óòõôöő]/ui', 'o', $fajlnev1);
-        $fajlnev1 = preg_replace('/[úùûüű]/ui', 'u', $fajlnev1);
-        $fajlnev1 = preg_replace('/[ç]/ui', 'c', $fajlnev1);
-        $fajlnev1 = preg_replace('/[^a-z0-9]/i', '_', $fajlnev1);
-        $fajlnev1 = preg_replace('/_+/', '_', $fajlnev1);
-        $fajlnev1 = strtolower($fajlnev1);
-        $fajlnev1 = $fajlnev1 . '_profil_' . $idopont1 . '.' . $kiterjesztes1;
+        //$id = $this->session->userdata('user')['user_id'];
 
-        $config1['upload_path']          = './uploads/';
-        $config1['allowed_types']        = 'gif|jpg|png|jpeg|bmp';
-        $config1['max_size']             = 10240;
-        $config1['file_name']            = $fajlnev1;
-
-        $this->load->library('upload', $config1);
-        
-		if (!$this->upload->do_upload('profilkep')) {
-			$this->session->set_flashdata('error', $this->upload->display_errors());
-			$this->session->set_flashdata('last_request', $this->input->post());
-			redirect('profil/profil_modositas');
-		}
-
+        //$id = $this->input->post('user_id');
 		$data = [
             'telepules_id' => $this->input->post('telepules_id'),
             'cim' => $this->input->post('cim'),
             'email' => $this->input->post('email'),
             'telszam' => $this->input->post('telszam'),
-            'profilkep' => $fajlnev1 
         ];
 
 
-        //$id = $this->session->userdata('user')['user_id'];
+        $this->load->model('user_model');
         $this->user_model->user_modositasa($id, $data);
-        $this->session->set_flashdata('success', "Sikeres adatmódosítás!");
+        $this->session->set_flashdata('success', "Sikeresen módosította profilját!");
         redirect('profil/profil_megtekintes');
 	}
 
