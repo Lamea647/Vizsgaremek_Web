bc28ff06219e661abc00ba10cd6cbc45b2a60896
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-08-28 00:20:16 +0200

    Betöltött profiladatok lekérdezésekből történő megjelenítése session helyett

--- application/views/modositott_profil.php
+++ application/views/modositott_profil.php
@@ -1,28 +1,28 @@
 <br>
 <div class="container"><h6 style="font-weight: bold; text-align: center;">Profil módosítása:</h6></div>
 <div class="container">
-    <form action="<?php echo base_url(). 'adatok_mod/' . $_SESSION['user']['user_id']; ?>" method="post" enctype="multipart/form-data">
+    <form action="<?php echo base_url()?>modositas/<?php echo $_SESSION['user']['user_id']; ?>" method="POST">
     <div class="form-group">
-            <label for="telepules_id">Település:</label>
-            <select class="form-control" id="telepules_id" name="telepules_id" required>
-            <?php for ($x = 0; $x < $telepules_szam; $x++):?>
-                <option value="
-                <?php if($x+1 == $_SESSION['user']['telepules_id']):?>
-                    <?php echo $x+1; ?>" selected>
-                <?php else: echo $x+1; ?>"><?php endif ?><?php echo $telepules[$x]['telepules']; ?><?php endfor ?></option> 
-            </select> 
+        <label for="telepules_id">Település:</label>
+        <select class="form-control" id="telepules_id" name="telepules_id" required>
+        <?php for ($x = 0; $x < $telepules_szam; $x++) {?>
+        <option value="<?php echo $x+1; ?>"><?php echo $telepules[$x]['telepules']; ?></option> <?php } ?>
+        </select> 
     </div>
     <div class="form-group">
             <label for="cim">Cím:</label>
-            <input type="text" class="form-control" id="cim" name="cim" maxlength="100" pattern=".{8,100}" required value="<?php echo $_SESSION['user']['cim']; ?>">
+            <input type="text" class="form-control" id="cim" name="cim" maxlength="100" pattern=".{8,100}" required value="<?php echo $userAdatai[0]['cim']; ?>">
     </div>
     <div class="form-group">
             <label for="email">E-mail cím:</label>
-            <input type="email" class="form-control" id="email" name="email" maxlength="100" required value="<?php echo $_SESSION['user']['email']; ?>">
+            <input type="email" class="form-control" id="email" name="email" maxlength="100" required value="<?php echo $userAdatai[0]['email']; ?>">
     </div>
     <div class="form-group">
             <label for="telszam">Telefonszám:</label>
-            <input type="tel" class="form-control" id="telszam" name="telszam" maxlength="30" pattern=".{7,30}" required value="<?php echo $_SESSION['user']['telszam']; ?>">
+            <input type="tel" class="form-control" id="telszam" name="telszam" maxlength="30" pattern=".{7,30}" required value="<?php echo $userAdatai[0]['telszam'];; ?>">
+    </div>
+    <div class="form-group">
+            <input type="hidden" class="form-control" id="user_id" name="user_id" value="<?php echo $_SESSION['user']['user_id']; ?>">
     </div>
     <button type="submit" class="btn btn-warning" value="true">Profil módosítása</button>
     </form>
@@ -44,6 +44,5 @@
       </div>
       <button type="submit" class="btn btn-warning" value="true">Jelszó módosítása</button>
 </div><br>
-
 
 
