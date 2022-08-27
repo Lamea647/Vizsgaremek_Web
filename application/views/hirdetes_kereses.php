781ada0fd26205734a16b222ae0645b32668ffb2
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-08-28 00:18:37 +0200

    Találatok dinamikus megjelenítése lekérdezések alapján, statikus kód törlése

--- application/views/hirdetes_kereses.php
+++ application/views/hirdetes_kereses.php
@@ -1,4 +1,5 @@
 <div class="container"><h5 style="font-weight: bold; text-align: center;">Hirdetések közötti keresés/szűrés:</h5></div>
+<form action="<?php echo base_url()?>kereses" method="POST">
 <div class="form-group">
     <label for="kategoria_id">Kategória szerinti szűrés:</label>
     <select class="form-control" id="kategoria_id" name="kategoria_id">
@@ -17,47 +18,28 @@
     <label for="kezdo_idopont">Dátum szerinti szűrés:</label>
     <input type="date" class="form-control" id="kezdo_idopont" name="kezdo_idopont">
 </div>
-<div><button type="button" class="btn btn-warning">Szűrés</button></div>
+<div><button type="submit" class="btn btn-warning" name="szures" value="true">Szűrés</button></div>
 <div class="container"><h5 style="font-weight: bold; text-align: center;">Találatok:</h5></div>
 
+<?php if(count($lekerdezettHirdetesek) == 0 && isset($_POST["szures"])){
+    echo "Nincs találat a megadott paraméterek alapján.";
+}?>
 <div class="row">
+    <?php for ($i=0; $i < count($lekerdezettHirdetesek) ; $i++) {?>
     <div class="col-sm-12 col-md-4 col-lg-3" style="margin-bottom: 10px;">
         <div class="card" style="width: 100%">
-            <img class="card-img-top" src="<?php echo base_url(); ?>images/kutyasetaltatas.jpg" alt="Card image" style="width:100%">
+            <img class="card-img-top" src="<?php echo base_url(); ?>images/<?php echo $kategoriaKep['kategoria_kep'];?>" alt="Card image" style="width:100%">
             <div class="card-body">
-            <p class="card-text" style="text-align: justify;"><?php echo "Ez egy szövegrészlet a hirdetés feladásakor kötelezően kitöltött, a hirdetést ismertető leírásból."?></p>
-            <a href="#" class="btn btn-warning">Tovább a hirdetésre</a>
+            <p class="card-text" style="text-align: justify;"><?php echo strlen($lekerdezettHirdetesek[$i]['leiras']) > 50 ? substr($lekerdezettHirdetesek[$i]['leiras'], 0, 47) . "..." : $lekerdezettHirdetesek[$i]['leiras']; ?></p>
+            <a href="<?php echo base_url(); ?>hirdetes/<?php echo $lekerdezettHirdetesek[$i]['hirdetes_id']; ?>" class="btn btn-warning">Tovább a hirdetésre</a>
             </div>
         </div>
     </div>
-    <div class="col-sm-12 col-md-4 col-lg-3" style="margin-bottom: 10px;">
-        <div class="card" style="width: 100%">
-            <img class="card-img-top" src="<?php echo base_url(); ?>images/vasarlas.jpg" alt="Card image" style="width:100%">
-            <div class="card-body">
-            <p class="card-text" style="text-align: justify;"><?php echo "Ez egy szövegrészlet a hirdetés feladásakor kötelezően kitöltött, a hirdetést ismertető leírásból."?></p>
-            <a href="#" class="btn btn-warning">Tovább a hirdetésre</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-12 col-md-4 col-lg-3" style="margin-bottom: 10px;">
-        <div class="card" style="width: 100%">
-            <img class="card-img-top" src="<?php echo base_url(); ?>images/takaritas.jpg" alt="Card image" style="width:100%">
-            <div class="card-body">
-            <p class="card-text" style="text-align: justify;"><?php echo "Ez egy szövegrészlet a hirdetés feladásakor kötelezően kitöltött, a hirdetést ismertető leírásból."?></p>
-            <a href="#" class="btn btn-warning">Tovább a hirdetésre</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-12 col-md-4 col-lg-3" style="margin-bottom: 10px;">
-        <div class="card" style="width: 100%">
-            <img class="card-img-top" src="<?php echo base_url(); ?>images/takaritas.jpg" alt="Card image" style="width:100%">
-            <div class="card-body">
-            <p class="card-text" style="text-align: justify;"><?php echo "Ez egy szövegrészlet a hirdetés feladásakor kötelezően kitöltött, a hirdetést ismertető leírásból."?></p>
-            <a href="#" class="btn btn-warning">Tovább a hirdetésre</a>
-            </div>
-        </div>
-    </div>
+    <?php } ?>
 </div>
+
+
+
 
 
 
