846bce01c84bdbe5cbd81aa080d7d4b873712abf
Gulácsi-Szabó Tímea <szabotimea89@gmailcom>
2022-08-28 00:10:51 +0200

    Hirdetes_kereses metódus fejlesztése, szükséges lekérdezések hozzáadása

--- application/controllers/Hirdetes_kereses.php
+++ application/controllers/Hirdetes_kereses.php
@@ -25,7 +25,18 @@ class Hirdetes_kereses extends CI_Controller {
 
         $telepules_szam = $this->telepules_model->telepulesekSzama();
         $data['telepules_szam'] = $telepules_szam;
+
+        $telepules_id = $this->input->post('telepules_id');
+        $kategoria_id = $this->input->post('kategoria_id');
+        $kezdo_idopont = $this->input->post('kezdo_idopont');
+        $user_id = $_SESSION['user']['user_id'];
         
+        $lekerdezettHirdetesek = $this->hirdetes_model->hirdetesKereses($telepules_id, $kategoria_id, $kezdo_idopont, $user_id);
+        $data['lekerdezettHirdetesek'] = $lekerdezettHirdetesek;
+
+        $kategoriaKep = $this->kategoria_model->get_by_id($kategoria_id);
+        $data['kategoriaKep'] = $kategoriaKep;
+
         $this->load->view('header', ['oldal' => 'hirdetesek_keresese']);
         $this->load->view('hirdetes_kereses', $data);
         $this->load->view('footer');
@@ -53,6 +64,7 @@ class Hirdetes_kereses extends CI_Controller {
         $this->load->view('header');
         $this->load->view('hirdetes', $data);
         $this->load->view('footer');
+    
     }
 
 }
