d59a8342f469b1bab412205a40a28c7b9d537855
Dénes Evelin <denes.evelin@gmail.com>
2022-09-05 18:06:04 +0200

    userAdatokModositasa metódus javítása A User_modelben

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -90,10 +90,10 @@ class User_model extends CI_Model {
     }
 
     public function userAdatokModositasa($telszam, $email, $telepules_id, $cim, $user_id){
-        $this->db->set('telszam', '$telszam');
-        $this->db->set('email', '$email');
-        $this->db->set('telepules_id', '$telepules_id');
-        $this->db->set('cim', '$cim');
+        $this->db->set('telszam', $telszam);
+        $this->db->set('email', $email);
+        $this->db->set('telepules_id', $telepules_id);
+        $this->db->set('cim', $cim);
         $this->db->where('user_id', $user_id);
         $this->db->update('user');
     }
